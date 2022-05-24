2d082bcff84209d26d4e7bacc1ca84ef4d693b8e
Bart <bart.roosen@vdabcampus.be>
2022-05-24 12:44:15 +0200

    add menu

--- js/app.js
+++ js/app.js
@@ -6,3 +6,37 @@ if ('serviceWorker' in navigator) {
             console.log('SW registered');
         });
 }
+
+
+const menuButton = document.getElementById('menu-button');
+const menuCard = document.getElementById('menu-card');
+let menuVisible = false;
+
+
+menuButton.addEventListener('click', function () {
+    if (menuVisible) {
+        move(0, -500, 'down');
+    } else {
+        move(-500, 0, 'up');
+    }
+    menuVisible = !menuVisible;
+});
+
+function move(from, to, direction) {
+    let id = null;
+    clearInterval(id);
+    id = setInterval(frame, 1);
+    function frame() {
+        if (from === to) {
+            clearInterval(id);
+        } else {
+            if ('up' === direction) {
+                from += 10;
+            } else {
+                from -= 10;
+            }
+
+            menuCard.style.left = from + "px";
+        }
+    }
+}
